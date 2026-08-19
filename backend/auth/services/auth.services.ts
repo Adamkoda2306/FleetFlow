150fb2cc1b7dc118ea6b20274d25aea9cf000302
AdamKoda2306 <adam.koda2306@gmail.com>
2026-08-20 03:28:03 +0530

    created the register service and implemented the grpc inter-communication between user and auth

--- backend/auth/services/auth.services.ts
+++ backend/auth/services/auth.services.ts
@@ -1,11 +1,51 @@
-import { LoginInterface } from "../interface/auth.interface";
-import { LoginServiceResponseInterface } from "../interface/auth.routes.interface";
+import { RegisterInterface } from "../interface/auth.interface";
+import { RegisterServiceResponseInterface } from "../interface/auth.routes.interface";
+import {
+    createUserGrpc,
+    getUserGrpc
+} from "../grpc/user.grpc";
+import { CreateUserResponse } from "../interface/grpc.interface";
+import { generateTokenUser } from "../utils/jwt.utils";
 
 
-const loginService = async (data: LoginInterface): Promise<LoginServiceResponseInterface> => {
-    
+const registerService = async (data: RegisterInterface): Promise<RegisterServiceResponseInterface> => {
+    try {
+        const result: CreateUserResponse = await createUserGrpc(data);
+        if (!result.success) {
+            // console.log('Error at registerService in auth.service due to grpc');
+            return result;
+        }
+        if (!result.id || !result.role) {
+            throw new Error('Internal Server Error!');
+        }
+
+        const accessToken = await generateTokenUser(result.id, result.role, false, false);
+        const refreshToken = await generateTokenUser(result.id, result.role, true, false);
+
+        if (!accessToken.success) {
+            return { statusCode: 500, success: false, message: accessToken.message };
+        }
+        if (!refreshToken.success) {
+            return { statusCode: 500, success: false, message: refreshToken.message };
+        }
+
+        return {
+            statusCode: result.statusCode,
+            success: result.success,
+            message: result.message,
+            accessToken: accessToken.token,
+            refreshToken: refreshToken.token
+        };
+    } catch (err: any) {
+        console.log('Error at registerService in auth.service :', err.message);
+        return {
+            statusCode: 500,
+            success: false,
+            message: 'Internal Server Error!'
+        };
+    }
 };
 
 export {
-    loginService
+    registerService
 };
